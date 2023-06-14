fd510d2e38b72a7263bc0c36952a5c57e768e76e
FletcherMan <fanciture@163.com>
2023-06-14 11:34:19 +0800

    bug fix

--- core/consensus_message.go
+++ core/consensus_message.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"github.com/morphism-labs/node/types"
 	"github.com/scroll-tech/go-ethereum/common"
-	eth "github.com/scroll-tech/go-ethereum/core/types"
 	"github.com/scroll-tech/go-ethereum/eth/catalyst"
 	"math/big"
 )
@@ -29,12 +28,15 @@ func (bc *Version1Converter) Separate(l2Block *catalyst.ExecutableL2Data, l1Msg 
 	copy(blsBytes[120:122], types.Uint16ToBigEndianBytes(uint16(len(l2Block.Transactions))))
 	copy(blsBytes[122:124], types.Uint16ToBigEndianBytes(uint16(0))) // later replace it to len(l1Msg)
 
-	rest := types.NonBLSMessage{
-		StateRoot:   l2Block.StateRoot,
-		GasUsed:     l2Block.GasUsed,
-		ReceiptRoot: l2Block.ReceiptRoot,
-		LogsBloom:   l2Block.LogsBloom,
-		L1Messages:  l1Msg,
+	rest := types.RestMessage{
+		NonBLSMessage: types.NonBLSMessage{
+			StateRoot:   l2Block.StateRoot,
+			GasUsed:     l2Block.GasUsed,
+			ReceiptRoot: l2Block.ReceiptRoot,
+			LogsBloom:   l2Block.LogsBloom,
+			L1Messages:  l1Msg,
+		},
+		Miner: l2Block.Miner,
 	}
 	restBytes, err := rest.MarshalBinary()
 	if err != nil {
@@ -47,8 +49,8 @@ func (bc *Version1Converter) Recover(blsMsg []byte, restMsg []byte, txs [][]byte
 	if len(blsMsg) != 124 {
 		return nil, nil, fmt.Errorf("wrong blsMsg size, expected: %d, actual: %d", 124, len(blsMsg))
 	}
-	nbm := new(types.NonBLSMessage)
-	if err := nbm.UnmarshalBinary(restMsg); err != nil {
+	rest := new(types.RestMessage)
+	if err := rest.UnmarshalBinary(restMsg); err != nil {
 		return nil, nil, err
 	}
 	if binary.BigEndian.Uint16(blsMsg[120:122]) != uint16(len(txs)) {
@@ -61,17 +63,17 @@ func (bc *Version1Converter) Recover(blsMsg []byte, restMsg []byte, txs [][]byte
 	return &catalyst.ExecutableL2Data{
 		Hash:         common.BytesToHash(blsMsg[:32]),
 		ParentHash:   common.BytesToHash(blsMsg[32:64]),
-		Miner:        eth.EmptyAddress,
+		Miner:        rest.Miner,
 		Number:       binary.BigEndian.Uint64(blsMsg[64:72]),
 		Timestamp:    binary.BigEndian.Uint64(blsMsg[72:80]),
 		BaseFee:      new(big.Int).SetBytes(blsMsg[80:112]),
 		GasLimit:     binary.BigEndian.Uint64(blsMsg[112:120]),
-		StateRoot:    nbm.StateRoot,
-		GasUsed:      nbm.GasUsed,
-		ReceiptRoot:  nbm.ReceiptRoot,
-		LogsBloom:    nbm.LogsBloom,
+		StateRoot:    rest.StateRoot,
+		GasUsed:      rest.GasUsed,
+		ReceiptRoot:  rest.ReceiptRoot,
+		LogsBloom:    rest.LogsBloom,
 		Transactions: txs,
-	}, nbm.L1Messages, nil
+	}, rest.L1Messages, nil
 }
 
 type Version2Converter struct{}
