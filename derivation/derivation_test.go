b03de41107fa75c14cd7268db4c326414e6b30ee
curryxbo <curryx1992@gmail.com>
2023-12-20 17:03:52 +0800

    clean useless code

--- derivation/derivation_test.go
+++ derivation/derivation_test.go
@@ -72,18 +72,6 @@ func TestFetchRollupData(t *testing.T) {
 	d := testNewDerivationClient(t)
 	_, err := d.fetchRollupDataByTxHash(common.HexToHash("0x27deefd6883567dec94fa3eca918de6a4a6d330e6f225b6282a5ab7e3c1a50dc"), 390)
 	require.NoError(t, err)
-	//logs, err := d.fetchRollupLog(context.Background(), 390, 390)
-	//require.NoError(t, err)
-	//for _, lg := range logs {
-	//	rollupData, err := d.fetchRollupDataByTxHash(lg.txHash, lg.BlockNumber)
-	//	if err != nil {
-	//		d.logger.Error("fetch rollup data failed", "txHash", lg.txHash, "blockNumber", lg.BlockNumber)
-	//		return
-	//	}
-	//	d.logger.Info("fetch rollup transaction success", "txNonce", rollupData.nonce, "txHash", rollupData.txHash,
-	//		"l1BlockNumber", rollupData.l1BlockNumber, "firstL2BlockNumber", rollupData.firstBlockNumber, "lastL2BlockNumber", rollupData.lastBlockNumber)
-	//
-	//}
 }
 
 func newSimulatedBackend(key *ecdsa.PrivateKey) (*backends.SimulatedBackend, ethdb.Database) {
