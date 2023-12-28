26145a26dd31dbb01535dd0e7d89f3fed40f47ed
curryxbo <curryx1992@gmail.com>
2023-12-28 21:18:03 +0800

    fix L1MessageReader and testcase

--- db/store_test.go
+++ db/store_test.go
@@ -53,10 +53,3 @@ func TestSyncedL1Messages(t *testing.T) {
 	msg = db.ReadL1MessageByIndex(200)
 	require.Nil(t, msg)
 }
-
-func TestStore_WriteLatestBatchBls(t *testing.T) {
-	db := NewMemoryStore()
-	var batchBls types.BatchBls
-	db.WriteLatestBatchBls(batchBls)
-	batchBls = db.ReadLatestBatchBls()
-}
